91e1c76c29caec4ff75d2e5c4b36a0cfe50e46b3
caraanJC <caraanjohncarlo@gmail.com>
2021-10-12 20:10:15 +0800

    feat: add checkPassword and changePassword route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const auth = require('./verifyToken');
 
+const bcrypt = require('bcrypt');
+
 const users = require('../models/users.model');
 
 router.get('/', auth, (req, res) => {
@@ -40,6 +42,33 @@ router.put('/changeAvatar', auth, (req, res) => {
             $set: { avatar: req.body.url },
         })
         .then((data) => res.send('Avatar Uploaded'));
+});
+
+router.post('/checkPassword', auth, (req, res) => {
+    users.findById(req.user._id).then(async (data) => {
+        const match = await bcrypt.compare(req.body.password, data.password);
+        if (match) {
+            res.send(true);
+        } else {
+            res.send(false);
+        }
+    });
+});
+
+router.put('/changePassword', auth, async (req, res) => {
+    try {
+        const passwordHash = await bcrypt.hash(req.body.password, 10);
+
+        users
+            .findByIdAndUpdate(req.user._id, {
+                $set: { password: passwordHash },
+            })
+            .then((data) => {
+                res.send('Changed User Password');
+            });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;
