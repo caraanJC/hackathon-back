cd3d75129983dc2948cf1ea3f0415b74ed444fe2
caraanJC <caraanjohncarlo@gmail.com>
2021-10-20 18:42:30 +0800

    feat: add items router

--- routes/items.js
+++ routes/items.js
@@ -1,3 +1,48 @@
 const router = require('express').Router();
+const auth = require('./verifyToken');
+
+const items = require('../models/items.model');
+const users = require('../models/users.model');
+
+router.get('/', auth, (req, res) => {
+    items.find().then((data) => {
+        res.send(data);
+    });
+});
+
+router.post('/addItem', auth, (req, res) => {
+    users.findById(req.user._id).then((data) => {
+        if (data.roles.includes('admin')) {
+            const newItem = new items(req.body);
+            newItem.save().then((data) => res.send('Item added'));
+        } else {
+            res.send('Access denied');
+        }
+    });
+});
+
+router.put('/:item_id/editItem', auth, (req, res) => {
+    users.findById(req.user._id).then((data) => {
+        if (data.roles.includes('admin')) {
+            items
+                .findByIdAndUpdate(req.params.item_id, req.body)
+                .then((data) => res.send('Item Edited'));
+        } else {
+            res.send('Access denied');
+        }
+    });
+});
+
+router.delete('/:item_id/deleteItem', auth, (req, res) => {
+    users.findById(req.user._id).then((data) => {
+        if (data.roles.includes('admin')) {
+            items
+                .findByIdAndDelete(req.params.item_id)
+                .then((data) => res.send('Item Deleted'));
+        } else {
+            res.send('Access denied');
+        }
+    });
+});
 
 module.exports = router;
